2cada544e801ec5cd5ca54da6fb20af965b98c51
Pratik Ratnaparkhi <pratikr557@gmail.com>
2016-05-26 00:13:28 +0530

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,25 @@
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		
+		//Give two examples of data that cannot be stored in a variable of type int.
+		
+		
+		
+		int a = 12.3;  // double cannot be stored in int. 
+		
+		int b = "name"; //String cannot be stored in int
+		
+		
+		
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
